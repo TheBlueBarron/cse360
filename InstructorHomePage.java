02962d222a061700499817003e28a9d23b905dd6
Dalton115 <148163756+Dalton115@users.noreply.github.com>
2025-02-07 03:45:25 -0700

    Update InstructorHomePage.java
    added comment about database

--- InstructorHomePage.java
+++ InstructorHomePage.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 
 public class InstructorHomePage {
 	
-	private final DatabaseHelper databaseHelper;
+	private final DatabaseHelper databaseHelper; //Added databasehelper for use in the future
 	
     public InstructorHomePage(DatabaseHelper databaseHelper) {
         this.databaseHelper = databaseHelper;  
