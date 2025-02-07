39bba00821a8e8006c1f2d55be7cd1f6a2db37bb
Dalton115 <148163756+Dalton115@users.noreply.github.com>
2025-02-07 03:45:43 -0700

    Update ReviewerHomePage.java
    databse comment

--- ReviewerHomePage.java
+++ ReviewerHomePage.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 
 public class ReviewerHomePage {
 	
-	private final DatabaseHelper databaseHelper;
+	private final DatabaseHelper databaseHelper; //Added databasehelper for use in the future
 	
     public ReviewerHomePage(DatabaseHelper databaseHelper) {
         this.databaseHelper = databaseHelper;  
