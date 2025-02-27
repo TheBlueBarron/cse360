24e597df0c0f322c9a49012b73c089cfa3e14b5a
radwanaj <81585994+radwanaj@users.noreply.github.com>
2025-02-26 23:43:46 -0700

    Update StartCSE360.java

--- StartCSE360.java
+++ StartCSE360.java
@@ -1,9 +1,11 @@
 package application;
 
-import databasePart1.DatabaseHelper; 
-import javafx.application.Application; 
-import javafx.stage.Stage; 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import java.sql.SQLException;
+
+import databasePart1.DatabaseHelper;
+
 
 public class StartCSE360 extends Application {
 
@@ -16,30 +18,18 @@ public class StartCSE360 extends Application {
 	
 	@Override
     public void start(Stage primaryStage) {
-//        try {
-//            databaseHelper.connectToDatabase(); // Connect to the database
-//            if (databaseHelper.isDatabaseEmpty()) {
-//            	
-//            	new FirstPage(databaseHelper).show(primaryStage);
-//            } else {
-//            	new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
-//                
-//            }
-//        } catch (SQLException e) {
-//        	System.out.println(e.getMessage());
-//        }
-		 try {
-		        databaseHelper.connectToDatabase(); // Connect to the database
-		        if (databaseHelper.isDatabaseEmpty()) {
-		        // Displaying the Discussion page instead of FirstPage for HW2
-		        new DiscussionPage(databaseHelper).show(primaryStage);
-		        } else {
-		        	new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
-	                
-	            }
-		    } catch (SQLException e) {
-		        System.out.println(e.getMessage());
-		    }
+        try {
+            databaseHelper.connectToDatabase(); // Connect to the database
+            if (databaseHelper.isDatabaseEmpty()) {
+            	
+            	new FirstPage(databaseHelper).show(primaryStage);
+            } else {
+            	new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
+                
+            }
+        } catch (SQLException e) {
+        	System.out.println(e.getMessage());
+        }
     }
 	
 
