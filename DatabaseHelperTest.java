c0958427834041b5df0e64de47e14bffdaa5be7d
Xpetty <xgpetty@asu.edu>
2025-04-04 22:07:50 -0700

    JUnit testing CRUD operations for Review

---
+++ DatabaseHelperTest.java
@@ -0,0 +1,113 @@
+package databasePart1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import application.Answer;
+import application.Question;
+import application.Review;
+
+class DatabaseHelperTest {
+	
+	private static DatabaseHelper databasehelper = new DatabaseHelper();
+    static Question q;
+    static Answer a;
+    static Review r1;
+    static Review r2;
+    static Review r3;
+    static Review newReview;
+    
+	@BeforeAll
+	static void setup ( ) throws SQLException {
+		try {
+			databasehelper.connectToDatabase();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Create Question, Answer, and Reviews once and reuse them across tests
+        q = new Question("Is this working?", "Xander", true);
+        databasehelper.addQuestion(q);
+        a = new Answer(q.getId(), "Sure is!", "Anonymous", true);
+        databasehelper.addAnswer(a);
+        r1 = new Review(a.getId(), "Very epic review", "Mr.Review");
+        r2 = new Review(a.getId(), "I like trains", "Steve");
+        r3 = new Review(a.getId(), "I like apples", "Adam");
+        newReview = new Review(a.getId(), "New Review", "Mr.Review");
+        databasehelper.addReview(r1);
+        databasehelper.addReview(r2);
+        databasehelper.addReview(r3);
+	}
+	
+	@Test
+	void testAddReview() throws SQLException {
+		databasehelper.addReview(newReview);
+		assertEquals("New Review", databasehelper.getReviewById(newReview.getId()).getText(), "The Text should be the same");
+	}
+	
+	@Test
+	void testGetReviewById() throws SQLException {
+		assertEquals("Very epic review", databasehelper.getReviewById(r1.getId()).getText(), "The Text should be the same");
+	}
+	
+	@Test
+	void testGetAllReviews() throws SQLException {
+		List<Review> reviewList = new ArrayList<>();
+		reviewList.add(r1);
+		reviewList.add(r2);
+		reviewList.add(r3);
+		
+        assertEquals(reviewList.get(0).getText(), databasehelper.getAllReviews().get(0).getText(), "Review 1 should be in the database");
+        assertEquals(reviewList.get(1).getText(), databasehelper.getAllReviews().get(1).getText(), "Review 2 should be in the database");
+        assertEquals(reviewList.get(2).getText(), databasehelper.getAllReviews().get(2).getText(), "Review 3 should be in the database");
+	}
+	
+	@Test
+	void testUpdateReviewText() throws SQLException {
+		databasehelper.updateReviewText(r1.getId(), "New Text");
+		assertEquals("New Text", databasehelper.getReviewById(r1.getId()).getText(), "The text should be the same.");
+		
+		databasehelper.updateReviewText(r1.getId(), "Very epic review");
+		assertEquals("Very epic review", databasehelper.getReviewById(r1.getId()).getText(), "The text should be the same.");
+	}
+	
+	@Test
+	void testDeleteReview() throws SQLException {
+		assertTrue(databasehelper.deleteReview(r1.getId()));
+	}
+	
+	@Test
+	void testSearchReviews() throws SQLException {
+		
+		List<Review> expectedResults = new ArrayList<>();
+		expectedResults.add(r2);
+		expectedResults.add(r3);
+		
+		List<Review> actualList = databasehelper.searchReviews("like", q.getId());
+		
+		assertEquals(expectedResults.get(0).getText(), actualList.get(0).getText(), "Review 2 should be in the database");
+        assertEquals(expectedResults.get(1).getText(), actualList.get(1).getText(), "Review 3 should be in the database");
+	}
+	
+	@Test
+	void testGetReviewsForAnswers() throws SQLException {
+		
+		List<Review> expectedResults = new ArrayList<>();
+		expectedResults.add(r1);
+		expectedResults.add(r2);
+		expectedResults.add(r3);
+		
+		List<Review> actualList = databasehelper.getReviewsForAnswers(a.getId());
+		
+		assertEquals(expectedResults.get(0).getText(), actualList.get(0).getText(), "Review 1 should be in the database");
+        assertEquals(expectedResults.get(1).getText(), actualList.get(1).getText(), "Review 2 should be in the database");
+        assertEquals(expectedResults.get(2).getText(), actualList.get(2).getText(), "Review 3 should be in the database");
+	}
+
+}
